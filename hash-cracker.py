bf88bfae21f33ef2ed432a6667d8179983c50cbc
itismhn <itismhn@yahoo.com>
2023-10-16 09:27:03 +0330

    define input func

--- hash-cracker.py
+++ hash-cracker.py
@@ -5,7 +5,10 @@ def main():
 ▀──▀ ▀──▀ ▀▀▀ ▀──▀ ── ▀▀▀ ▀─▀▀ ▀──▀ ▀▀▀ ▀─▀ ▀▀▀ ▀─▀▀
 @itisMHN v1.1
 """)
-    pass
+    user_input()
+def user_input():
+    print("[1] Hash Identifier\n[2] Hash Cracker")
+    input_option = input("Please Enter an Option: ")
 
 if __name__ == "__main__":
     main()
