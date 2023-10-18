cee76386bc40921f14c19e9cb9e787a94e1e90d2
itismhn <itismhn@yahoo.com>
2023-10-18 18:15:58 +0330

    adding digit input

--- hash-cracker.py
+++ hash-cracker.py
@@ -30,7 +30,6 @@ def user_input():
             print("\n [!] Exiting...")
     except Exception as err:
         print("[!] Got Error : ", err)
-
 def hash_identifier(input_hash):
     md5_valid_hash = "9b63036e1089e21b8a599bdfb720b7da"
     sha1_valid_hash = "3b3a15497ad565a6045a1649d13114d625a93283"
@@ -46,17 +45,21 @@ def hash_identifier(input_hash):
         print("[!] Got Error!")
 def hash_cracker(input_hash):
     print('----------')
-    print("[- choose an word-List to crack the hash -]")
+    print("[++] choose an word-List to crack the hash")
     print("[-1-] number-list\n[-2-] Your word-list file")
     list_option = input("[*] Please Enter a list: ")
     if list_option == '1':
-                # main crack function
-                pass
+        print('----------')
+        print("[+++] for exaple enter 4 for testing (0000 - 9999)")
+        number_digit = input("Enter a digit: ")
+        digit_option = input("[*] Please Enter a list: ")
+        pass
     elif list_option == '2':
         print("coming soon ...")
     else:
         print("\n[!] Choose Correct Option!\n")
         sleep(0.7)
-
+def main_cracker(input_hash,crack_list_option):
+    print(input_hash)
 if __name__ == "__main__":
     main()
