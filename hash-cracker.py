333b27b0dc0ee9cd127480604b749bea416be9bb
itismhn <itismhn@yahoo.com>
2023-10-19 10:16:01 +0330

    improve identifier func

--- hash-cracker.py
+++ hash-cracker.py
@@ -17,10 +17,12 @@ def user_input():
             input_option = input("[+] Please Enter an Option: ")
             sleep(0.7)
             if input_option == '1':
-                hash_identifier(input_hash)
+                print("[-] Hash is ")
+                print(hash_identifier(input_hash))
                 break
             elif input_option == '2':
-                hash_cracker(input_hash)
+                print("[-] Hash is ")
+                print(hash_cracker(input_hash))
                 break
             else:
                 print("\n[!] Choose Correct Option!\n")
@@ -36,30 +38,31 @@ def hash_identifier(input_hash):
     sleep(0.7)
     try:
         if(len(input_hash) == len(md5_valid_hash)):
-            print('[-] Hash is MD5')
+            return('MD5')
         elif(len(input_hash) == len(sha1_valid_hash)):
-            print('[-] Hash is SHA1')
+            return('SHA1')
         else:
             print("[-] can't detect hash")
     except:
         print("[!] Got Error!")
 def hash_cracker(input_hash):
-    print('----------')
-    print("[++] choose an word-List to crack the hash")
-    print("[-1-] number-list\n[-2-] Your word-list file")
-    list_option = input("[*] Please Enter a list: ")
-    if list_option == '1':
+    try:
         print('----------')
-        print("[+++] for exaple enter 4 for testing (0000 - 9999)")
-        number_digit = input("Enter a digit: ")
-        digit_option = input("[*] Please Enter a list: ")
-        pass
-    elif list_option == '2':
-        print("coming soon ...")
-    else:
-        print("\n[!] Choose Correct Option!\n")
-        sleep(0.7)
-def main_cracker(input_hash,crack_list_option):
+        print("[++] choose an word-List to crack the hash")
+        print("[-1-] number-list\n[-2-] Your word-list file")
+        list_option = input("[*] Please Enter a list: ")
+        if list_option == '1':
+            print('----------')
+            print("[+++] for example enter 4 for testing (0000 - 9999)")
+            number_digit = int(input("[+] Enter a digit: "))
+        elif list_option == '2':
+            print("coming soon ...")
+        else:
+            print("\n[!] Choose Correct Option!\n")
+            sleep(0.7)
+    except TypeError as err:
+        print(f"Got {err}\n Enter valid number")
+def main_cracker(input_hash,hash_digit):
     print(input_hash)
 if __name__ == "__main__":
     main()
